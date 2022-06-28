7d2451f5dc3aaaba2378f91f84ca20a857124ade
peng <pengzhang40@gmail.com>
2022-06-27 21:38:24 -0700

    minor change to "about us"

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -4,16 +4,21 @@ import "./index.scss";
 function AboutUs() {
   return (
     <div className='about-us'>
-      Little Sprouts is San Diego’s best Reggio preschool offering organic meals
-      and serving the greater San Diego area including Poway, Mission Valley, La
-      Mesa, Allied Gardens and Kensington. Our preschool was created to provide
-      enriching, quality care through play-based learning. We offer a clean and
-      inviting environment and healthy, organic food will always be served.
-      Whether you need full-time or part-time care, you will find that Little
-      Sprouts Academy is the perfect place for your child. Feel free to browse
-      our interactive website to learn more about the best preschool and daycare
-      in San Diego! You can contact us anytime and we would love to meet you and
-      your Little Sprout!
+      <h1>
+        About Us
+      </h1>
+      <p>
+        Little Sprouts is San Diego's best Reggio preschool offering organic meals
+        and serving the greater San Diego area including Poway, Mission Valley, La
+        Mesa, Allied Gardens and Kensington. Our preschool was created to provide
+        enriching, quality care through play-based learning. We offer a clean and
+        inviting environment and healthy, organic food will always be served.
+        Whether you need full-time or part-time care, you will find that Little
+        Sprouts Academy is the perfect place for your child. Feel free to browse
+        our interactive website to learn more about the best preschool and daycare
+        in San Diego! You can contact us anytime and we would love to meet you and
+        your Little Sprout!
+      </p>
     </div>
   );
 }
