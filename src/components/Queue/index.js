af9be948ee4f4a7666bcb8c689c3843e4ee1dada
Zixu Yu <sheldon@Zixus-MBP-2.lan>
2022-06-29 16:18:13 -0700

    user inforamtion form

--- src/components/Queue/index.js
+++ src/components/Queue/index.js
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.scss";
 
 const Queue = () => {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    name: "",
+    age: "",
+    gender: "",
+    dipper: false,
+  });
+
   return (
     <div className='queueContainer'>
-      <h1>Enter your child Info to queue up today!</h1>
-      <input type='text' placeholder='Email' />
-      <input type='text' placeholder='Name' />
-      <input type='text' placeholder='Age' />
-      <input type='text' placeholder='Gender' />
-      <input type='text' placeholder='Dipper' />
-      <input type='text' />
+      <h1>Schedule a visit</h1>
 
+      <form action='' className='userInfo'>
+        <div className='userInfoContainer'>
+          <div className='wrapper'>
+            <div className='title'>Email</div>
+            <input type='text' placeholder='Email' />
+          </div>
+        </div>
+
+        <div className='userInfoContainer'>
+          <div className='wrapper'>
+            <div className='title'>Kid's Name</div>
+            <input type='text' placeholder='Name' />
+          </div>
+        </div>
+
+        <div className='userInfoContainer'>
+          <div className='wrapper'>
+            <div className='title'>Kid's Age</div>
+            <input type='text' placeholder='Age' />
+          </div>
+        </div>
+
+        <div className='userInfoContainer'>
+          <div className='wrapper'>
+            <div className='title'>Kid's Gender</div>
+            <input type='text' placeholder='Gender' />
+          </div>
+        </div>
+
+        <div className='userInfoContainer'>
+          <div className='wrapper'>
+            <div className='title'>Dipper usage</div>
+            <input type='dropdown' placeholder='Dipper' />
+          </div>
+        </div>
+      </form>
+
+      <button className='submitForm'>Submit</button>
       <p>Our staff will contact you shortly</p>
     </div>
   );
